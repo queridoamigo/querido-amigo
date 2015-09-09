2c5430daf222a7c8ed20ea8de338ba2d25f85cb9
Сергей Марчук <Smarchuk@MacBook-Pro-Sergej-2.local>
2015-09-09 18:23:06 +0300

    more php

--- homepage/index.php
+++ homepage/index.php
@@ -2,9 +2,9 @@
 <html>
 
   <head>
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
-    <link rel="stylesheet" href="CSS/main.css">
+	<?php
+		include("includes/links.php");	
+	?>
     <title>Querido_Amigo</title>
   </head>
 
@@ -37,51 +37,50 @@
       </div>
     </div> 
 
-    <div class="neighborhood-guides">
+    <div class="main-pics">
         <div class="container">
             <h2>Shots about my life</h2>
             <p></p>
-            <div class="row">
-                <div class="col-md-4">
-                      <div class="thumbnail">
-					<a class="cboxElement" rel="colorbox" href="https://farm1.staticflickr.com/506/19401739228_248e7fa5a7_c.jpg" style="outline: none"> <img src="https://farm1.staticflickr.com/506/19401739228_248e7fa5a7_n.jpg" style="border: 1px solid #456;" alt="Lorem ipsum dolor sit amet..." />
-					</a> 
-                      </div>
-                      <div class="thumbnail">
-					<a class="cboxElement" rel="colorbox" href="https://farm1.staticflickr.com/264/20151032216_40dfb8280f_c.jpg" style="outline: none" > <img src="https://farm1.staticflickr.com/264/20151032216_40dfb8280f_n.jpg" style="border: 1px solid #456;" alt="Lorem ipsum dolor sit amet..." />
-					</a> 
-                      </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-                        <!-- <img src="https://farm1.staticflickr.com/264/20151032216_40dfb8280f_n.jpg" />
-                    -->
-					<a class="cboxElement" rel="colorbox" href="https://farm6.staticflickr.com/5822/19997280023_a37cc32e73_c.jpg" style="outline: none" > <img src="https://farm6.staticflickr.com/5822/19997280023_a37cc32e73_n.jpg" style="border: 1px solid #456;" alt="Lorem ipsum dolor sit amet..." />
-					</a> 
-				</div>
-                    <div class="thumbnail">
-					<a class="cboxElement" rel="colorbox" href="https://farm1.staticflickr.com/506/19828357310_5d5789d621_c.jpg" style="outline: none" > <img src="https://farm1.staticflickr.com/506/19828357310_5d5789d621_n.jpg" style="border: 1px solid #456;" alt="Lorem ipsum dolor sit amet..." />
-					</a> 
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="thumbnail">
-					<a class="cboxElement" rel="colorbox" href="https://farm1.staticflickr.com/538/20485737746_a0771c2943_c.jpg" style="outline: none" > <img src="https://farm1.staticflickr.com/538/20485737746_a0771c2943_n.jpg" style="border: 1px solid #456;" alt="Lorem ipsum dolor sit amet..." />
-					</a> 
-                    </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <div class="thumbnail">
-					<a class="cboxElement" rel="colorbox" href="https://farm4.staticflickr.com/3771/19552191951_a431197caf_c.jpg" style="outline: none" > <img src="https://farm4.staticflickr.com/3771/19552191951_a431197caf_n.jpg" style="border: 1px solid #456;" alt="Lorem ipsum dolor sit amet..." />
-					</a> 
-                    </div>
-                </div>
-                
-            </div>
+				<?php
+						completeRows();
+				?>                
         </div>
     </div>
 
+<?php
+
+function completeRows() {
+
+	$linksPics[0] = array("thumb" => "https://farm1.staticflickr.com/506/19401739228_248e7fa5a7_n.jpg", "fullsize" => "https://farm1.staticflickr.com/506/19401739228_248e7fa5a7_c.jpg");
+	$linksPics[1] = array("thumb" => "https://farm1.staticflickr.com/264/20151032216_40dfb8280f_n.jpg", "fullsize" => "https://farm1.staticflickr.com/264/20151032216_40dfb8280f_c.jpg");
+	$linksPics[2] = array("thumb" => "https://farm1.staticflickr.com/506/19828357310_5d5789d621_n.jpg", "fullsize" => "https://farm1.staticflickr.com/506/19828357310_5d5789d621_c.jpg");
+	$linksPics[3] = array("thumb" => "https://farm6.staticflickr.com/5822/19997280023_a37cc32e73_n.jpg", "fullsize" => "https://farm6.staticflickr.com/5822/19997280023_a37cc32e73_c.jpg");
+	$linksPics[4] = array("thumb" => "https://farm1.staticflickr.com/538/20485737746_a0771c2943_n.jpg", "fullsize" => "https://farm1.staticflickr.com/538/20485737746_a0771c2943_c.jpg");
+	$linksPics[5] = array("thumb" => "https://farm4.staticflickr.com/3771/19552191951_a431197caf_n.jpg", "fullsize" => "https://farm4.staticflickr.com/3771/19552191951_a431197caf_c.jpg");
+
+	$rows = 
+            "
+            <div class=\"row\"> ";
+
+	foreach($linksPics as $row => $value) {
+			$rows .=" 
+					<div class='col-md-4'>
+							<div class='thumbnail'>
+							<a class='cboxElement' rel='colorbox' href='". $value['fullsize'] ."' style='outline: none' > <img src='". $value['thumb'] ."' style='border: 1px solid #456;' alt='Lorem ipsum dolor sit amet...' />
+							</a> 
+							</div>
+					</div>
+					";
+	}
+
+	$rows .= " 
+				</div> 
+				";
+
+	echo $rows;
+}
+
+?>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="app.js"></script>
 	<script src="jquery.colorbox-min.js"></script>
