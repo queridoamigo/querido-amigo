aa6bd8a8b8e5141511e154816abb9b51fc9f49c3
Сергей Марчук <Smarchuk@MacBook-Pro-Sergej-2.local>
2015-09-10 16:47:34 +0300

    more links

--- homepage/includes/links.php
+++ homepage/includes/links.php
@@ -4,3 +4,6 @@ ini_set('error_log', 'php_errors.log');
 ?>
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="CSS/main.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
